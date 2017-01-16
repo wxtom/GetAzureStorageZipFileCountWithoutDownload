6ae72c402b1344ad5e3789008c3e95c5d0d90cbf
FAREAST\v-tsu <v-tsu@microsoft.com>
2017-01-16 18:48:43 +0800

    master

--- TestGzip/Program.cs
+++ TestGzip/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System.IO.Compression;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace TestGzip
 {
@@ -10,6 +9,25 @@ namespace TestGzip
     {
         static void Main(string[] args)
         {
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("connectionstring");
+
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer container = blobClient.GetContainerReference("testcont");
+
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference("azureCredential.zip");
+
+            using (var memoryStream = new MemoryStream())
+            {
+                blockBlob.DownloadToStream(memoryStream);
+                using (ZipArchive zip = new ZipArchive(memoryStream))
+                {
+                    var count = zip.Entries.Count;
+                }
+            }
+
         }
     }
 }
+
